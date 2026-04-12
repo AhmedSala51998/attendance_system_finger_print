2124e662768edd95736c63d0a8d42cc999441b20
git <ahmedsalah511998@gmail.com>
2026-04-12 17:37:53 +0300

    ghgfjghj

--- layout/header.php
+++ layout/header.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Font Awesome الأحدث -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
     <!-- ✅ المسار الذكي لضمان عمل التصميم في كل مكان -->
     <?php 

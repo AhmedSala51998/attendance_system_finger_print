ac93684f7afb386cb39550d9c90bf008c545531c
git <ahmedsalah511998@gmail.com>
2026-04-16 11:33:36 +0300

    لاتالتال

--- employee/leave_requests.php
+++ employee/leave_requests.php
@@ -65,8 +65,11 @@ if(isset($_POST['add_leave'])){
     ");
     $stmt->bind_param("iss", $emp_id, $from_date, $to_date);
     $stmt->execute();
-    $stmt->bind_result($perm_count);
-    $stmt->fetch();
+    $result = $stmt->get_result();
+
+    $row = $result->fetch_row();
+    $perm_count = $row[0] ?? 0;
+
     $stmt->close();
 
     if ($perm_count > 0) {
